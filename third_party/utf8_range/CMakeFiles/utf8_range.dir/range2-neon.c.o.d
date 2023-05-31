e111aa185a586f2cff94b520d1df64e7aeb7dc08
Takashi Kamada <sara1203rkwr@zeus.eonet.ne.jp>
2023-05-31 20:35:21 +0900

    first commit

---
+++ third_party/utf8_range/CMakeFiles/utf8_range.dir/range2-neon.c.o.d
@@ -0,0 +1,3 @@
+third_party/utf8_range/CMakeFiles/utf8_range.dir/range2-neon.c.o: \
+ /home/kamada/protobuf/protobuf/third_party/utf8_range/range2-neon.c \
+ /usr/include/stdc-predef.h
---
+++ third_party/utf8_range/CMakeFiles/utf8_range.dir/range2-sse.c.o.d
@@ -0,0 +1,3 @@
+third_party/utf8_range/CMakeFiles/utf8_range.dir/range2-sse.c.o: \
+ /home/kamada/protobuf/protobuf/third_party/utf8_range/range2-sse.c \
+ /usr/include/stdc-predef.h
